2fb8cbb38be318e575d363ff681128d61be80ba1
aliaramli <jaliascv@gmail.com>
2020-03-24 09:29:56 +0000

    add basic blog mvc modules,tagging,image cover

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 # Create your models here.
+
+STATUS = (
+        (0, "Draft"),
+        (1, "Pending"),
+        (2, "Publish")
+        )
+
+class Post(models.Model):
+    # GOOGLE will cut off too long title length search result
+    # Thus i set as 60 char.
+    title = models.CharField(max_length=60, unique=True)
+    # Shorter slug also easy to be processed by GOOGLE
+    slug  = models.SlugField(max_length=60, unique=True)
+    author = models.ForeignKey (User, on_delete=models.CASCADE, related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    cover = models.ImageField(upload_to='images/', default='images/default.png')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now = True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    tags = TaggableManager()
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=2).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+def tagged(request, slug):
+    tag = get_object_or_404(Tag, slug=slug)
+    # Filter posts by tag name  
+    posts = Post.objects.filter(tags=tag)
+    context = {
+        'tag':tag,
+        'posts':posts,
+    }
+    return render(request, 'index.html', context)
+
